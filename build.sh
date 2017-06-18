0ec5ae8b20d03c9f2a3b1e9ca761356a5d297a23
Florian Kromer <thinwybk@mailbox.org>
2017-06-18 23:41:33 +0200

    add: install doxygen in script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 . venv/doxinabox/bin/activate  # source does only work with bash
 cd doc/ext/doxygen/
+sudo apt install doxygen
 doxygen sphinx
 cd ../../..
 sphinx-build -b html ./doc/source ./doc/build
